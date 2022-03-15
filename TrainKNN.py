be7b31f462b837b6df5850162864e907337344df
sona <quentin.raymondaud84700@gmail.com>
2022-03-15 22:00:27 +0100

    Save compressed data - Dataset Feature Remove __pycache__ from repo

--- TrainKNN.py
+++ TrainKNN.py
@@ -2,13 +2,13 @@ from Trainer import Trainer
 
 class TrainKNN(Trainer):
 
-    def __init__(self, path):
-        super().__init__(path)
+    def __init__(self, path, save_gzip_path=None):
+        super().__init__(path, save_gzip_path=save_gzip_path)
 
     def train(self):
         print("Please implement the train function first in TrainKNN!")
         self.model = None
         raise NotImplementedError
 
-s = TrainKNN("data/projects.csv")
+s = TrainKNN("data/projects.csv", save_gzip_path="data/knn-prepro")
 s.train()
