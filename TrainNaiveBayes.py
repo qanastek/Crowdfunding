72e0a7d75804385ad184362f2c15a137de98c97b
Labrak Yanis <yanis.labrak@alumni.univ-avignon.fr>
2022-03-15 23:03:15 +0100

    Add NaïveBayes

--- TrainNaiveBayes.py
+++ TrainNaiveBayes.py
@@ -1,14 +1,22 @@
 from Trainer import Trainer
+from sklearn.naive_bayes import GaussianNB
 
 class TrainNaiveBayes(Trainer):
 
-    def __init__(self, path):
+    def __init__(self, path, save_gzip_path=None, clean_gzip=False):
         super().__init__(path)
+        self.model = GaussianNB()
 
     def train(self):
-        print("Please implement the train function first in TrainNaiveBayes!")
-        self.model = None
-        raise NotImplementedError
 
-s = TrainNaiveBayes("data/projects.csv")
+        self.model.fit(self.ds.x_train, self.ds.y_train)
+
+        res = self.predict()
+        print(res)
+
+        print("Score train: ", self.model.score(self.ds.x_train, self.ds.y_train))
+        print("Score test: ", self.model.score(self.ds.x_test, self.ds.y_test))
+
+s = TrainNaiveBayes("data/short.csv", save_gzip_path="data/bayes-prepro")
+# s = TrainNaiveBayes("data/projects.csv", save_gzip_path="data/bayes-prepro")
 s.train()
