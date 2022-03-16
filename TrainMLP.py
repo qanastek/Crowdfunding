b4890131f1ae2491b2abc348350baebaddf7e295
Labrak Yanis <yanis.labrak@alumni.univ-avignon.fr>
2022-03-16 13:59:09 +0100

    MLP sklearn

--- TrainMLP.py
+++ TrainMLP.py
@@ -1,25 +1,35 @@
 from Trainer import Trainer
 
-# class MLP:
-
-
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
 
 class TrainMLP(Trainer):
+    """
+    Trainer for the MLP
+    """
 
-    def __init__(self, path, save_gzip_path=None, clean_gzip=False):
-        super().__init__(path)
-        self.model = None
+    def __init__(self, path, shuffle=True, seed=0, save_gzip_path=None, clean_gzip=False, normalizer="StandardScaler"):
+        super().__init__(path, shuffle=shuffle, seed=seed, save_gzip_path=save_gzip_path, clean_gzip=clean_gzip, normalizer=normalizer)
+        self.EPOCHS = 999999999
 
     def train(self):
 
-        self.model.fit(self.ds.x_train, self.ds.y_train)
+        m = MLPClassifier(
+            random_state=1,
+            max_iter = 1,
+            # learning_rate = 0.01,
+        )
 
-        res = self.predict()
-        print(res)
+        print("> START TRAINING !")
+        self.model = m.fit(self.ds.x_train, self.ds.y_train)
 
-        print("Score train: ", self.model.score(self.ds.x_train, self.ds.y_train))
-        print("Score test: ", self.model.score(self.ds.x_test, self.ds.y_test))
+        print("Score train: ", m.score(self.ds.x_train, self.ds.y_train))
+        print("Score test: ", m.score(self.ds.x_test, self.ds.y_test))
 
-s = TrainMLP("data/short.csv", save_gzip_path="data/mlp-prepro")
-# s = TrainMLP("data/projects.csv", save_gzip_path="data/mlp-prepro")
+# s = TrainMLP("data/short.csv", normalizer="StandardScaler")
+# s = TrainMLP("data/short.csv", normalizer="MinMaxScaler")
+# s = TrainMLP("data/short.csv", normalizer=None)
+s = TrainMLP("data/projects.csv", normalizer="StandardScaler")
 s.train()
+f1 = s.evaluate()
+print(f1)
