d051087a92dd75835736ddb46f1c5c0b9b80e5e2
PhiltasticGuy <philippe.turcotte.pt@gmail.com>
2022-03-16 22:48:13 +0100

    Created Decision Tree.

--- TrainDecisionTree.py
+++ TrainDecisionTree.py
@@ -1,34 +1,32 @@
 from Trainer import Trainer
 
-from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import GridSearchCV
 
-class TrainSVM(Trainer):
+class TrainDecisionTree(Trainer):
     """
-    Trainer for the SVM
+    Trainer for the DecisionTreeClassifier
     """
 
     def __init__(self, path, shuffle=True, seed=0, save_gzip_path=None, clean_gzip=False, normalizer="StandardScaler"):
-        super().__init__(path, shuffle=shuffle, seed=seed, save_gzip_path=save_gzip_path, clean_gzip=clean_gzip, normalizer=normalizer)
+        super().__init__(path, shuffle=shuffle, seed=seed, save_gzip_path=save_gzip_path, clean_gzip=clean_gzip, normalizer=normalizer, train_ratio=0.75)
         self.EPOCHS = 999999999
 
     def train(self):
 
-        svm = LinearSVC(
-            max_iter = 20,
-            # learning_rate = 0.01,
-        )
+        classifier = DecisionTreeClassifier(max_depth=10)
+        # classifier = DecisionTreeClassifier()
 
         print("> START TRAINING !")
-        self.model = svm.fit(self.ds.x_train, self.ds.y_train)
+        self.model = classifier.fit(self.ds.x_train, self.ds.y_train)
 
-        print("Score train: ", svm.score(self.ds.x_train, self.ds.y_train))
-        print("Score test: ", svm.score(self.ds.x_test, self.ds.y_test))
+        print("Score train: ", classifier.score(self.ds.x_train, self.ds.y_train))
+        print("Score test: ", classifier.score(self.ds.x_test, self.ds.y_test))
 
 # s = TrainSVM("data/short.csv", normalizer="StandardScaler")
 # s = TrainSVM("data/short.csv", normalizer="MinMaxScaler")
 # s = TrainSVM("data/short.csv", normalizer=None)
-s = TrainSVM("data/projects.csv", normalizer="StandardScaler")
+s = TrainDecisionTree("data/projects.csv", normalizer="StandardScaler")
 s.train()
 f1 = s.evaluate()
 print(f1)
